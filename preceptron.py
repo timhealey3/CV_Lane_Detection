538170ee76c1e285f32efa296ee79fbf293efc40
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2024-06-11 23:13:16 -0400

    Prediction preceptron

--- preceptron.py
+++ preceptron.py
@@ -27,3 +27,25 @@ model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
 # shuffle, shuffles the data before each epoch to help fight against gettincaught in local min.
 h = model.fit(x=X, y=y, verbose=1, batch_size=50, epochs = 500, shuffle='true')
 
+def plot_decision_boundary(X, y, model):
+    x_span = np.linspace(min(X[:, 0]) - 1, max(X[:, 0]) + 1)
+    y_span = np.linspace(min(X[:,1]) - 1, max(X[:, 1]) + 1)
+    xx, yy = np.meshgrid(x_span, y_span)
+    xx_, yy_ = xx.ravel(), yy.ravel()
+    grid = np.c_[xx_, yy_]
+    pred_func = model.predict(grid)
+    z = pred_func.reshape(xx.shape)
+    plt.contour(xx, yy, z)
+
+plot_decision_boundary(X, y, model)
+plt.scatter(X[:n_pts,0], X[:n_pts,1])
+plt.scatter(X[n_pts:,0], X[n_pts:,1])
+x = 7.5
+y = 5
+     
+     
+point = np.array([[x, y]])
+prediction = model.predict(point)
+plt.plot([x], [y], marker='o', markersize=10, color="red")
+print("prediction is: ",prediction)
+
